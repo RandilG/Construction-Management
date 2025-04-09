926ba3688cb7c4c3f4ae18a5fdd18421c988897c
Randil G Dissanayake <dissanayakewrg.21@uom.lk>
2025-04-09 23:40:24 +0530

    DB changed

--- SW_Mobile_App-main/src/screens/OtpVerification.js
+++ SW_Mobile_App-main/src/screens/OtpVerification.js
@@ -60,7 +60,7 @@ const OTPVerification = () => {
     }
 
     try {
-      const response = await axios.post('http://192.168.8.116:3000/api/verify-otp', {
+      const response = await axios.post('http://192.168.8.116:8081/api/verify-otp', {
         email,
         otp: otpCode
       });
@@ -78,7 +78,7 @@ const OTPVerification = () => {
 
   const handleResendOtp = async () => {
     try {
-      const response = await axios.post('http://192.168.8.175:3000/api/resend-otp', {
+      const response = await axios.post('http://192.168.8.116:8081/api/resend-otp', {
         email
       });
 
